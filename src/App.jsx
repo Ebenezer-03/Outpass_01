2aacd4cc32c1342c4d73bf7c5613efada2f1aa5f
Ebenezer-03 <itzmeebenezer2007@gmail.com>
2025-05-13 22:59:36 +0530

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import LoginSignup from './Components/LoginSignup.jsx'
 
 const App = () => {
   return (
-    <div>App
-      <h1>Hello World</h1>
-      <p>This is a simple React application.</p>
-      <p>This application works on the generation of Outpass to College Hostel Students.</p>
-      <p>Enjoy coding!</p>
+    <div>
+      <h1>Welcome to the App</h1>
+      <LoginSignup />
     </div>
   )
 }
