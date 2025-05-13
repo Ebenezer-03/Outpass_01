05595afc5b5603f165246130eb4077d512092d0f
Ebenezer-03 <itzmeebenezer2007@gmail.com>
2025-05-13 21:31:02 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App
+      <h1>Hello World</h1>
+      <p>This is a simple React application.</p>
+      <p>It uses Vite for development and build.</p>
+      <p>Enjoy coding!</p>
+    </div>
+  )
+}
+
+export default App
