0852532117d02aa883df0c8e959e066c08511ea6
Ebenezer-03 <itzmeebenezer2007@gmail.com>
2025-05-18 00:02:54 +0530

    Dasboard Creation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import RoleSelector from "./components/RoleSelector";
 import ValidateUser from "./components/ValidateUser";
 import "./App.css";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 function App() {
   const [selectedRole, setSelectedRole] = useState("");
@@ -13,7 +14,12 @@ function App() {
       <RoleSelector selectedRole={selectedRole} setSelectedRole={setSelectedRole} />
       <p>Selected Role: {selectedRole || "None"}</p>
       <ValidateUser role={selectedRole} />
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path = '/RoleSelection' element = {<RoleSelector selectedRole={selectedRole} setSelectedRole={setSelectedRole} />}/>
+        <Route path = '/Validate' element = {<ValidateUser role={selectedRole} />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
   
